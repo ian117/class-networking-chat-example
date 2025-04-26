e5a798f1be53fa819d7a0e85a1eab5806782d8f6
ian117 <spideramerica71@gmail.com>
2025-04-25 21:30:41 -0600

    fix: added CorsConfig to proyect

---
+++ src/main/java/network_class/chat_backend/security/CorsConfig.java
@@ -0,0 +1,24 @@
+package network_class.chat_backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 🔥 Permite a todos los endpoints
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500") // 🔥 Tu frontend local
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
